ddc626297927d843f0143390de7882f35f052d2e
Igor Shuvaev <i.schuwaew@gmail.com>
2026-01-19 17:56:29 +0300

    feat: new aliases

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -1,9 +1,14 @@
 
 alias ..='cd ..'
-alias rm='rm -i'
+alias rm='trash -v'
+alias mkdir='mkdir -p'
+alias cpp='rsync --info=progress2'
 alias sb='source ~/.bashrc'
 alias c='clear'
 alias ipconfig='ip addr show'
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {l}' --preview-window=right:60% | xargs -ro yay -S"
+
+alias ll='ls -Fls'
 
 alias gs='git status'
 alias ga='git add'
@@ -21,3 +26,11 @@ alias mkenv='python3 -m venv venv'
 alias avenv='source venv/bin/activate'
 alias djrun='python manage.py runserver'
 alias djmigrate='python manage.py migrate'
+
+s () {
+  local server
+  server=$(grep -E '^Host ' ~/.ssh/config | awk '{print $2}' | fzf)
+  if [[ -n $server ]] then
+    ssh $server
+  fi
+}
